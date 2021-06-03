e3b93f33a1e1fb9e435eb4b06adf36a32684574d
hupeijie <hupeijie@brilliance.com.cn>
2021-06-03 09:45:40 +0800

    第一次提交

---
+++ src/main/pjhuc01/test27/ProxyPattern.java
@@ -0,0 +1,39 @@
+package main.pjhuc01.test27;
+
+public class ProxyPattern {
+    public static void main(String[] args) {
+        new RealSubjectProxy().doWork();
+    }
+}
+interface Subject{
+    void doWork();
+}
+class RealSubject implements Subject{
+    @Override
+    public void doWork() {
+    
+    }
+}
+class RealSubjectProxy implements Subject{
+    private RealSubject subject;
+    
+    public RealSubjectProxy() {
+        try {
+            this.subject = (RealSubject) this.getClass().getClassLoader().loadClass("main.pjhuc01.test27.RealSubject").newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void connect(){
+        System.out.println("建立连接");
+    }
+    public void log(){
+        System.out.println("日志记录");
+    }
+    @Override
+    public void doWork() {
+        connect();
+        subject.doWork();
+        log();
+    }
+}
